1d3da9334c6dbdb0ad7ddbafe1f46ec547e5e819
fajarh8 <fajar0heryan@gmail.com>
2025-03-10 08:23:30 +0700

    add template layout per page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', function () {return view('index');});
+Route::get('/about', function () {return view('about');});
+Route::get('/destination', function () {return view('destination');});
+Route::get('/blog', function () {return view('blog');});
+Route::get('/blog-single', function () {return view('blog-single');});
+Route::get('/contact', function () {return view('contact');});
+Route::get('/destination', function () {return view('destination');});
+Route::get('/hotel', function () {return view('hotel');});
+Route::get('/main', function () {return view('main');});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
