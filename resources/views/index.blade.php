371d678a9ccb485b3d0a174766c24b96d63a92c8
fajarh8 <fajar0heryan@gmail.com>
2025-03-12 14:34:24 +0700

    new home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
 		<div class="container">
 			<div class="row no-gutters slider-text js-fullheight align-items-center" data-scrollax-parent="true">
 				<div class="col-md-7 ftco-animate">
-					<span class="subheading">Welcome to Pacific</span>
-					<h1 class="mb-4">Discover Your Favorite Place with Us</h1>
-					<p class="caps">Travel to the any corner of the world, without going around in circles</p>
+					<span class="subheading">Selamat Datang</span>
+					<h1 class="mb-4 text-uppercase">Website Informasi Seputar Pendakian</h1>
+					<p class="caps">infopendakian.com</p>
 				</div>
 				<a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex align-items-center justify-content-center mb-4">
 					<span class="fa fa-play"></span>
@@ -24,9 +24,9 @@
 						<div class="row">
 							<div class="col-md-12 nav-link-wrap">
 								<div class="nav nav-pills text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-									<a class="nav-link active mr-md-1" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">Search Tour</a>
+									{{-- <a class="nav-link active mr-md-1" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">Cari Gunung</a> --}}
 
-									<a class="nav-link" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">Hotel</a>
+									{{-- <a class="nav-link" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">Kota</a> --}}
 
 								</div>
 							</div>
@@ -37,54 +37,52 @@
 									<div class="tab-pane fade show active" id="v-pills-1" role="tabpanel" aria-labelledby="v-pills-nextgen-tab">
 										<form action="#" class="search-property-1">
 											<div class="row no-gutters">
-												<div class="col-md d-flex">
+                                                <div class="col-md d-flex">
 													<div class="form-group p-4 border-0">
-														<label for="#">Destination</label>
+														<label for="#">Nama Gunung</label>
 														<div class="form-field">
 															<div class="icon"><span class="fa fa-search"></span></div>
-															<input type="text" class="form-control" placeholder="Search place">
+															<input type="text" class="form-control" placeholder="Cari Gunung">
+														</div>
+													</div>
+												</div>
+												<div class="col-md d-flex">
+													<div class="form-group p-4 border-0">
+														<label for="#">Kota</label>
+														<div class="form-field">
+															<div class="icon"><span class="fa fa-search"></span></div>
+															<input type="text" class="form-control" placeholder="Cari Kota">
 														</div>
 													</div>
 												</div>
 												<div class="col-md d-flex">
 													<div class="form-group p-4">
-														<label for="#">Check-in date</label>
+														<label for="#">Tanggal Pendakian</label>
 														<div class="form-field">
 															<div class="icon"><span class="fa fa-calendar"></span></div>
-															<input type="text" class="form-control checkin_date" placeholder="Check In Date">
+															<input type="text" class="form-control checkin_date" placeholder="Tanggal Pendakian">
 														</div>
 													</div>
 												</div>
 												<div class="col-md d-flex">
 													<div class="form-group p-4">
-														<label for="#">Check-out date</label>
+														<label for="#">Ketinggian</label>
 														<div class="form-field">
 															<div class="icon"><span class="fa fa-calendar"></span></div>
-															<input type="text" class="form-control checkout_date" placeholder="Check Out Date">
+															<input type="text" class="form-control" placeholder="Ketinggian Maks">
 														</div>
 													</div>
 												</div>
 												<div class="col-md d-flex">
 													<div class="form-group p-4">
-														<label for="#">Price Limit</label>
+														<label for="#">Harga Tiket</label>
 														<div class="form-field">
 															<div class="select-wrap">
 																<div class="icon"><span class="fa fa-chevron-down"></span></div>
 																<select name="" id="" class="form-control">
-																	<option value="">$100</option>
+																	<option value="">Harga Tiket Maks</option>
 																	<option value="">$10,000</option>
 																	<option value="">$50,000</option>
-																	<option value="">$100,000</option>
-																	<option value="">$200,000</option>
-																	<option value="">$300,000</option>
-																	<option value="">$400,000</option>
-																	<option value="">$500,000</option>
-																	<option value="">$600,000</option>
-																	<option value="">$700,000</option>
-																	<option value="">$800,000</option>
-																	<option value="">$900,000</option>
-																	<option value="">$1,000,000</option>
-																	<option value="">$2,000,000</option>
 																</select>
 															</div>
 														</div>
@@ -93,7 +91,7 @@
 												<div class="col-md d-flex">
 													<div class="form-group d-flex w-100 border-0">
 														<div class="form-field w-100 align-items-center d-flex">
-															<input type="submit" value="Search" class="align-self-stretch form-control btn btn-primary">
+															<input type="submit" value="Cari" class="align-self-stretch form-control btn btn-primary">
 														</div>
 													</div>
 												</div>
@@ -101,7 +99,7 @@
 										</form>
 									</div>
 
-									<div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-performance-tab">
+									{{-- <div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-performance-tab">
 										<form action="#" class="search-property-1">
 											<div class="row no-gutters">
 												<div class="col-lg d-flex">
@@ -160,13 +158,13 @@
 												<div class="col-lg d-flex">
 													<div class="form-group d-flex w-100 border-0">
 														<div class="form-field w-100 align-items-center d-flex">
-															<input type="submit" value="Search" class="align-self-stretch form-control btn btn-primary p-0">
+															<input type="submit" value="Cari" class="align-self-stretch form-control btn btn-primary p-0">
 														</div>
 													</div>
 												</div>
 											</div>
 										</form>
-									</div>
+									</div> --}}
 								</div>
 							</div>
 						</div>
